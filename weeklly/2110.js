021eecb68698f9b0732e599f21a4043810a92ce7
trinhnhathuu <schoolgame2002@gmail.com>
2022-02-07 15:43:14 +0700

    first commit

---
+++ weeklly/2110.js
@@ -0,0 +1,12 @@
+let v=document.getElementsByClassName("container")  
+console.log(v);
+
+let t = document.getElementsByTagName("div")
+console.log(t);
+
+
+var one = document.querySelector('h1')
+console.log(one);
+
+document.write("hữu cười ")
+
